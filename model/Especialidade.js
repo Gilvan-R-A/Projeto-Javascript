3b58a21041d0d053f75e8ad4b56383522bf90eb5
Gilvan Alves <gilvan.dev@gmail.com>
2021-02-03 00:46:52 -0300

    Criando a classe especialidade em javascript

---
+++ model/Especialidade.js
@@ -0,0 +1,7 @@
+export default class Especialidade{
+    constructor(idEspecialidade, nomeEspecialidade, categoriaEspecialidade){
+        this.idEspecialidade = idEspecialidade;
+        this.nomeEspecialidade = nomeEspecialidade;
+        this.categoriaEspecialidade = categoriaEspecialidade;
+    }
+}
